a89a697b903a66cd3480a0ab06f26fc2604781ca
sethupathi <sethupathipalanisamyt@gmail.com>
2024-06-24 21:34:11 -1200

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace R2h_Erp_App.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
